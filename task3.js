1c5e785cc73c8131b153b51d29e27859514239e6
xioFelix <xiofelix@163.com>
2023-03-20 23:52:02 +1030

    8

---
+++ task3.js
@@ -0,0 +1,30 @@
+// 3.1
+let mcount = 0;
+document.getElementById("mcount").onmouseover = function () {
+    mouseCount();
+};
+function mouseCount() {
+    mcount++;
+    document.getElementById("mcount").innerHTML = mcount;
+}
+
+// 3.2
+function creatPost() {
+    const date = new Date();
+    // let date = d.toLocaleString();
+
+var posts = document.getElementById("posts");
+
+    let date_p = document.createElement('P');
+    let content_p = document.createElement('P');
+
+    date_p.classList.add("date");
+    content_p.classList.add("content");
+
+
+    date_p.innerText = date;
+    content_p.innerText = document.getElementById("input-content").value;
+
+    posts.appendChild(date_p);
+    posts.appendChild(content_p);
+}
