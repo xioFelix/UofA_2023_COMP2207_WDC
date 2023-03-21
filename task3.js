98dc1f64dbd9a5f6e83c37bbdd975bb942be6dc6
xioFelix <xiofelix@163.com>
2023-03-21 14:45:21 +1030

    14

--- task3.js
+++ task3.js
@@ -33,15 +33,15 @@ function creatPost() {
 let main = document.getElementById("main");
 let menu = document.getElementById("menu");
 
-main.addEventListener("click", foldMain);
-menu.addEventListener("click", foldMenu);
+main.addEventListener("click", fsMenu);
+menu.addEventListener("click", fsMenu);
 
-function foldMain() {
-    main.style.display = "none";
-    menu.style.display = "block";
-}
-
-function foldMenu() {
-    main.style.display = "block";
-    menu.style.display = "none";
+function fsMenu() {
+    if(menu.style.display === "none"){
+        menu.style.display = "block";
+        main.style.display = "none";
+    }else{
+        menu.style.display = "none";
+        main.style.display = "block";
+    }
 }
