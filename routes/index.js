c7785aa89d2df29ec7ffd255f5997971ded1db81
xioFelix <xiofelix@163.com>
2023-03-29 21:46:16 +1030

    11

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
 var timeStamp = null;
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'express'});
+  res.render('index', { title: 'express' });
 });
 
 router.get('/last.txt', (req, res) => {
-  console.log(`Last access: ${timeStamp}`);
-  timeStamp = new Date().toISOString();
   res.send(timeStamp);
+  timeStamp = new Date().toISOString();
 });
 
-module.exports=router;
+module.exports = router;
