dd63d77f3e91f579372e7a42a69fd55fa3894e7f
xioFelix <xiofelix@163.com>
2023-03-30 15:47:00 +0000

    Invalid DateTime

--- routes/index.js
+++ routes/index.js
@@ -54,17 +54,4 @@ router.get('/log.html', (req, res) => {
   res.send(html);
 });
 
-  // 4.1
-  function loadTime() {
-    alert("Load the function loadTime!");
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-      document.getElementById("lastTime").innerHTML = this.responseText;
-    };
-    xhttp.open("GET", "/last.txt");
-    xhttp.send();
-    alert("Load the function loadTime!");
-  }
-
-
 module.exports = router;
