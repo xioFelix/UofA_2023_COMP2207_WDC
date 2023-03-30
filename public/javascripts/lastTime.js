797e51abfea7add6c55b9c2f184375ba0ba1d7e7
xioFelix <xiofelix@163.com>
2023-03-30 16:04:14 +0000

    Invalid DateTime

--- public/javascripts/lastTime.js
+++ public/javascripts/lastTime.js
@@ -5,6 +5,6 @@
     xhttp.onload = function () {
       document.getElementById("lastTime").innerHTML = this.responseText;
     };
-    xhttp.open("GET", "./last.txt");
+    xhttp.open("GET", "http://localhost:8080/last.txt", true);
     xhttp.send();
   }
