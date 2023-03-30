a5c3bf9fa8c74991c47774a1208a8db58a83113b
xioFelix <xiofelix@163.com>
2023-03-30 16:30:29 +0000

    Invalid DateTime

--- public/javascripts/lastTime.js
+++ public/javascripts/lastTime.js
@@ -1,14 +1,15 @@
   // 4.1
   function lastTime() {
-
+    document.getElementById("lastTime").innerHTML = "Hello";
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
       var timeP = document.getElementById("lastTime");
+      xhttp.open("GET", "./last.txt", true);
+      xhttp.send();
       timeP.innerHTML = "Fail to Change";
       timeP.innerHTML = this.responseText;
 
     };
-    xhttp.open("GET", "./last.txt", true);
-    xhttp.send();
+
     console.log("Successfully use the lasttime function!");
   }
