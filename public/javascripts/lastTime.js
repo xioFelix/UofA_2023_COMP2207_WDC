94de2eb02175e567d32aa8e77128585c63b4f3d3
xioFelix <xiofelix@163.com>
2023-03-30 16:37:48 +0000

    Invalid DateTime

--- public/javascripts/lastTime.js
+++ public/javascripts/lastTime.js
@@ -2,10 +2,10 @@
   function lastTime() {
     document.getElementById("lastTime").innerHTML = "Hello";
     const xhttp = new XMLHttpRequest();
-    router.onload = function () {
+    xhttp.onload = function () {
       var timeP = document.getElementById("lastTime");
-      router.open("GET", "./last.txt", true);
-      router.send();
+      xhttp.open("GET", "./last.txt", true);
+      xh.send();
       timeP.innerHTML = "Fail to Change";
       timeP.innerHTML = this.responseText;
       console.log("Successfully use the xhttp function!");
