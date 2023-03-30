345894659054a5674095520c56873cee7a308f77
xioFelix <xiofelix@163.com>
2023-03-30 16:50:04 +0000

    Invalid DateTime

--- public/javascripts/lastTime.js
+++ public/javascripts/lastTime.js
@@ -5,8 +5,18 @@
     xhttp.open("GET", "./last.txt", true);
     xhttp.send();
     xhttp.onload = function () {
-      var timeP = document.getElementById("lastTime").innerHTML = this.responseText;
-      console.log("Successfully use the xhttp function!");
+      if (xhttp.status === 200) {
+        var timeP = document.getElementById("lastTime");
+        if (this.responseText) {
+          timeP.innerHTML = this.responseText;
+          console.log("Successfully use the xhttp function!");
+        } else {
+          console.log("Response is empty");
+        }
+      } else {
+        console.log("Request failed with status " + xhttp.status);
+      }
     };
     console.log("Successfully use the lasttime function!");
   }
+
