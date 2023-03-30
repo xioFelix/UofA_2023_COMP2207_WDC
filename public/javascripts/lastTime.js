5a51f1480584d11a7bdfdcf661ad57625908ebc5
xioFelix <xiofelix@163.com>
2023-03-30 23:34:43 +0000

    Invalid DateTime

--- public/javascripts/lastTime.js
+++ public/javascripts/lastTime.js
@@ -1,23 +1,13 @@
-  // 4.1
-  function lastTime() {
-    document.getElementById("lastTime").innerHTML = "Hello";
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "./last.txt", true);
-    xhttp.send();
-    xhttp.onload = function () {
-      if (xhttp.status === 200) {
-        var timeP = document.getElementById("lastTime");
-        if (this.responseText) {
-          timeP.innerHTML = this.responseText;
-          console.log("Successfully use the xhttp function!");
-        } else {
-          console.log("Response is empty");
-        }
-      } else {
-        console.log("Request failed with status " + xhttp.status);
-      }
-    };
-    console.log("Successfully use the lasttime function!");
-  }
-
-
+// 4.1
+function lastTime() {
+  document.getElementById("lastTime").innerHTML = "Hello";
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "./last.txt", true);
+  xhttp.send();
+  xhttp.onload = function () {
+    if (xhttp.status === 200) {
+      var timeP = document.getElementById("lastTime");
+      timeP.innerHTML = this.responseText;
+    }
+  };
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 var express = require('express');
 var router = express.Router();
 var timeStamp = null;
