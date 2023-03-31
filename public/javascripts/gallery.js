73793d32dc3cac8da5af42499e42b4a61f46ae73
xioFelix <xiofelix@163.com>
2023-03-31 16:30:34 +0000

    Update

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -5,7 +5,7 @@ function changeImage() {
     .then(data => {
       const imageElement = document.getElementById('src');
       const des = document.getElementById('alt-text');
-      imageElement.src = `../images/${data.uri}`;
+      imageElement.src = `./images/${data.uri}`;
       imageElement.alt = data.description;
       des.innerText = data.description;
     });
