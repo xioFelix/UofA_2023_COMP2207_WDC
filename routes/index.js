a74fcb82f49d9e03e8994484bbc596cebda223b9
xioFelix <xiofelix@163.com>
2023-03-31 04:15:42 +0000

    Invalid DateTime

--- routes/index.js
+++ routes/index.js
@@ -63,8 +63,25 @@ router.get('/color.txt', (req, res, next) => {
   changeColor();
   function changeColor() {
     colorIndex = colorIndex % (colors.length);
+
     res.sendFile('../public/color2.html');
+
+    function color() {
+      let color2 = new XMLHttpRequest();
+      color2.onreadystatechange = function () {
+        if (color2.readyState == 4 && color2.status == 200) {
+          let h1 = document.getElementById("changeColor");
+          h1.innerText = color2.response;
+          h1.style.color = color2.response;
+        }
+      };
+      color2.open("GET", `color.html`);
+      color2.send();
+    }
+    color();
+
   }
-});
+}
+);
 
 module.exports = router;
