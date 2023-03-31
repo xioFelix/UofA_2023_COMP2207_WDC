60ac70553f659d863d18acfe9d756489daabe0b4
xioFelix <xiofelix@163.com>
2023-03-31 16:24:59 +0000

    Invalid DateTime

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,16 +1,16 @@
 function changeImage() {
 
-    fetch('/images.json')
-      .then(response => response.json())
-      .then(data => {
-        const imageElement = document.getElementById('src');
-        const descriptionElement = document.getElementById('alt-text');
-        imageElement.src = `/images/${data.uri}`;
-        imageElement.alt = data.description;
-        descriptionElement.innerText = data.description;
-      })
-      .catch(data => console.log(data));
-  }
+  fetch('/images.json')
+    .then(response => response.json())
+    .then(data => {
+      const imageElement = document.getElementById('src');
+      const descriptionElement = document.getElementById('alt-text');
+      imageElement.src = `/images/${data.uri}`;
+      imageElement.alt = data.description;
+      descriptionElement.innerText = data.description;
+    })
+    .catch(data => console.log(data));
+}
 
-  changeImage();
-  setInterval(changeImage, 10000);
+changeImage();
+setInterval(changeImage, 10000);
