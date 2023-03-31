42efc6419c51801033dc8996d39e516733a40dc8
xioFelix <xiofelix@163.com>
2023-03-31 17:55:52 +0000

    Invalid DateTime

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,20 +1,15 @@
 function changeImage() {
-  const images = document.getElementById("src");
-  const description = document.getElementById("alt-text");
 
-  const image = new XMLHttpRequest();
-  image.onreadystatechange = function () {
-    if (image.readyState === XMLHttpRequest.DONE) {
-      if (image.status === 200) {
-        const data = JSON.parse(image.responseText);
-        images.src = `./images/${data.uri}`;
-        images.alt = data.description;
-        description.innerText = data.description;
-      }
-    }
-  };
-  image.open("GET", "/images.json", true);
-  image.send();
+  fetch('/images.json')
+    .then(response => response.json())
+    .then(data => {
+      const imageElement = document.getElementById('src');
+      const descriptionElement = document.getElementById('alt-text');
+      imageElement.src = `./images/${data.uri}`;
+      imageElement.alt = data.description;
+      descriptionElement.innerText = data.description;
+    })
+    .catch(data => console.log(data));
 }
 
 changeImage();
