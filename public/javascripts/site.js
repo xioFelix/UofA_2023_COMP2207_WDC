9bf6b1228f6859a2dd48f6684e8830a2b1cb1343
xioFelix <xiofelix@163.com>
2023-03-31 12:25:16 +0000

    Invalid DateTime

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,63 +1,25 @@
-// const contentDiv = document.querySelector('#content');
-// const contactBtn = document.querySelector('#contact');
-// const searchBtn = document.querySelector('#search');
-// const aboutBtn = document.querySelector('#about');
+const contentDiv = document.querySelector('#content');
+const contactBtn = document.querySelector('#contact');
+const searchBtn = document.querySelector('#search');
+const aboutBtn = document.querySelector('#about');
 
-// function loadPage(url) {
-//   fetch(url)
-//     .then(response => response.text())
-//     .then(html => contentDiv.innerHTML = html)
-//     .catch(error => console.log(error));
-// }
-
-// contactBtn.addEventListener('click', () => {
-//   loadPage('/contact.ajax');
-// });
-
-// searchBtn.addEventListener('click', () => {
-//   loadPage('/search.ajax');
-// });
-
-// aboutBtn.addEventListener('click', () => {
-//   loadPage('/about.ajax');
-// });
-
-
-// 4.4
-
-// contact
-function contact(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        if(this.readyState == 4 && this.status == 200){
-            document.getElementById("contact").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET",'/contact.ajax', true);
-    xhttp.send();
+function loadPage(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(html => contentDiv.innerHTML = html)
+    .catch(error => console.log(error));
 }
 
-// search
-function search(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        if(this.readyState == 4 && this.status == 200){
-            var button1 = document.getElementById("search");
-            button1.innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET",'/search.ajax');
-    xhttp.send();
-}
+contactBtn.addEventListener('click', () => {
+  loadPage('/contact.ajax');
+});
 
-// about
-function about(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        if(this.readyState == 4 && this.status == 200){
-            document.getElementById("about").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET",'/about.ajax');
-    xhttp.send();
-}
+searchBtn.addEventListener('click', () => {
+  loadPage('/search.ajax');
+});
+
+aboutBtn.addEventListener('click', () => {
+  loadPage('/about.ajax');
+});
+
+\
