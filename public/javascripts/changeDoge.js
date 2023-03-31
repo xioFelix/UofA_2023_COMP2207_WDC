2a41728c2e2bd70856b798873c7bb773f58c262f
xioFelix <xiofelix@163.com>
2023-03-31 13:30:59 +0000

    Invalid DateTime

---
+++ public/javascripts/changeDoge.js
@@ -0,0 +1,22 @@
+// 4.6
+function changeImage() {
+    const image = document.getElementById("image");
+    const description = document.getElementById("description");
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                image.src = "images/" + data.uri;
+                image.alt = data.description;
+                description.innerText = data.description;
+            }
+        }
+    };
+    xhr.open("GET", "/images.json", true);
+    xhr.send();
+}
+
+changeImage();
+setInterval(changeImage, 10000);
--- routes/index.js
+++ routes/index.js
@@ -112,27 +112,4 @@ router.get('/images.json', (req, res) => {
   res.json(photo);
 });
 
-//4.6
-function changeImage() {
-    const image = document.getElementById("image");
-    const description = document.getElementById("description");
-
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-     if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-       const data = JSON.parse(xhr.responseText);
-       image.src = "images/"+data.uri;
-       image.alt = data.description;
-       description.innerText = data.description;
-      }
-     }
-    };
-    xhr.open("GET", "/images.json", true);
-    xhr.send();
-   }
-
-  changeImage();
-  setInterval(changeImage, 10000);
-
 module.exports = router;
