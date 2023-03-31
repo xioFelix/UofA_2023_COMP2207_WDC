f161ca50dcf6d2cbfa91b06688ed01516ef54164
xioFelix <xiofelix@163.com>
2023-03-31 17:01:45 +0000

    Invalid DateTime

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,10 +1,9 @@
 function changeImage() {
 
   fetch('/images.js')
-    .then(response => response.js())
     .then(data => {
-      const imageElement = document.getElementById("src");
-      const altText = document.getElementById("alt-text");
+      let imageElement = document.getElementById("src");
+      let altText = document.getElementById("alt-text");
       imageElement.src = `/images/${data.uri}`;
       imageElement.alt = data.description;
       altText.innerText = data.description;
