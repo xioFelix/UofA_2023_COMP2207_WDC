17cd841336d04c2dae579c46809bcf909cac94f6
xioFelix <xiofelix@163.com>
2023-03-31 03:13:28 +0000

    Invalid DateTime

--- public/javascripts/color2.js
+++ public/javascripts/color2.js
@@ -1,13 +1,13 @@
-function color(){
-    let color2=new XMLHttpRequest();
-    color2.onreadystatechange=function(){
-        if(color2.readyState==4 && color2.status==200){
-            let h1=document.getElementById("changeColor");
-            h1.innerText=color2.response;
-            h1.style.color=`${color2.response}`;
+function color() {
+    let color2 = new XMLHttpRequest();
+    color2.onreadystatechange = function () {
+        if (color2.readyState == 4 && color2.status == 200) {
+            let h1 = document.getElementById("changeColor");
+            h1.innerText = color2.response;
+            h1.style.color = `${color2.response}`;
         }
     };
-    color2.open("GET",'/color.txt');
+    color2.open("GET", '/color.txt');
     color2.send();
 }
 color();
