f5b484f654f34b7a0c49ebc5440902d2bda5a0a7
xioFelix <xiofelix@163.com>
2023-03-31 16:13:40 +0000

    Update

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -3,8 +3,8 @@ function changeImage() {
     fetch('/images.json')
       .then(response => response.json())
       .then(data => {
-        const imageElement = document.getElementById('image');
-        const descriptionElement = document.getElementById('description');
+        const imageElement = document.getElementById('src');
+        const descriptionElement = document.getElementById('alt-text');
         imageElement.src = `/images/${data.uri}`;
         imageElement.alt = data.description;
         descriptionElement.innerText = data.description;
