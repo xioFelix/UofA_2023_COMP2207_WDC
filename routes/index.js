3b94c424d323d42ff0bb989c0cb9aabd81cc8e40
xioFelix <xiofelix@163.com>
2023-03-31 18:26:55 +0000

    Update

--- routes/index.js
+++ routes/index.js
@@ -80,7 +80,7 @@ router.get('/color.txt', (req, res) => {
 // 4.4
 router.get('/contact.ajax', (req, res) => {
   const link = '<a href = " ">contact us</ a>';
-  res.send(emailLink);
+  res.send(link);
 });
 
 router.get('/search.ajax', (req, res) => {
