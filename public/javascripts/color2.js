3d9139b594187cf4f97d881ba4c53064362490b0
xioFelix <xiofelix@163.com>
2023-03-31 02:48:40 +0000

    Invalid DateTime

--- public/javascripts/color2.js
+++ public/javascripts/color2.js
@@ -2,7 +2,7 @@ function color(){
     let color2=new XMLHttpRequest();
     color2.onreadystatechange=function(){
         if(color2.readyState==4 && color2.status==200){
-            let h1=document.getElementById("heading");
+            let h1=document.getElementById("changeColor");
             h1.innerText=color2.response;
             h1.style.color=color2.response;
         }
