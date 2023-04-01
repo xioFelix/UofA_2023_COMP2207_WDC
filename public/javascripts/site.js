01369291a74e10eb1a714bb4c16caef80e958146
xioFelix <xiofelix@163.com>
2023-04-01 14:44:02 +0000

    Final

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,17 +1,40 @@
+const contentDiv = document.querySelector('#content');
+
 function loadFunction(url) {
   fetch(url)
     .then(response => response.text())
     .then(html => document.querySelector('#content').innerHTML = html);
 }
 
-function contact(){
-  loadFunction('/contact.ajax');
+function contact() {
+  const contact = new XMLHttpRequest();
+  contact.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      contentDiv.innerHTML = this.responseText;
+    }
+  };
+  contact.open("GET", '/contact.ajax', true);
+  contact.send();
 }
 
-function search(){
-  loadFunction('/search.ajax');
+function search() {
+  const search = new XMLHttpRequest();
+  search.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      contentDiv.innerHTML = this.responseText;
+    }
+  };
+  search.open("GET", '/search.ajax', true);
+  search.send();
 }
 
-function about(){
-  loadFunction('/about.ajax');
+function about() {
+  const about = new XMLHttpRequest();
+  about.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      contentDiv.innerHTML = this.responseText;
+    }
+  };
+  about.open("GET", '/about.ajax', true);
+  about.send();
 }
