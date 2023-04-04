3bc2d1f9c2000042d445e5a68a29e9cb6d1e0828
xioFelix <xiofelix@163.com>
2023-04-04 12:30:25 +0000

    Apr 4, 2023, 22:00:25 GMT+9:30

--- query3-5.sql
+++ query3-5.sql
@@ -2,4 +2,4 @@ SELECT Students.given_name, Students.family_name
 FROM Students
 INNER JOIN Enrolments
 ON Enrolments.student_id = Students.student_id
-WHERE Enrolments.subject_code = 'COMP SCI 2207';
+WHERE Enrolments.mark < 50;
