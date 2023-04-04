ddf9cd4689f71827728e0866c2c0194a4a1027f1
xioFelix <xiofelix@163.com>
2023-04-04 13:02:58 +0000

    Apr 4, 2023, 22:32:58 GMT+9:30

--- query3-9.sql
+++ query3-9.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT Subjects.teacher_id, Teachers.given_name, Teachers.family_name
+SELECT Subjects.teacher_id, Teachers.given_name, Teachers.family_name
 FROM Enrolments
 INNER JOIN Subjects ON Enrolments.subject_code = Subjects.subject_code
 INNER JOIN Teachers ON Subjects.teacher_id = Teachers.staff_id
