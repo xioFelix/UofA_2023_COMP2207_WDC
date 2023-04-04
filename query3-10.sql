7468fd38c016bcc6dab3c46b9c6a9cea3dda7f18
xioFelix <xiofelix@163.com>
2023-04-04 13:10:44 +0000

    Apr 4, 2023, 22:40:44 GMT+9:30

--- query3-10.sql
+++ query3-10.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT Students.given_name, Students.family_name, Students.program, Subjects.subject, Subjects.teacher_id
 FROM Enrolments
 INNER JOIN Students ON Enrolments.student_id = Students.student_id
-INNER JOIN Subjects ON  Subjects.subject_code
+INNER JOIN Subjects ON Enrolments.subject_code = Subjects.subject_code
 WHERE Subjects.faculty <> 'ECMS';
