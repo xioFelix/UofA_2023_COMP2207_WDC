086663198696a427349aca369fe06787e466998a
xioFelix <xiofelix@163.com>
2023-04-04 07:02:23 +0000

    Invalid DateTime

--- query3-4.sql
+++ query3-4.sql
@@ -1,5 +1,5 @@
 SELECT student_id, students.first_name, students.last_name
 FROM Enrolments
 WHERE subject_code = 'COMP SCI 2207';
-INNER JOIN students
+INNER JOIN Students
 ON registration.student_id = students.student_id;
