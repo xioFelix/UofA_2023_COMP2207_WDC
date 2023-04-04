d513fa97f59d8a44f52b08548b82e99b2316788d
xioFelix <xiofelix@163.com>
2023-04-04 12:41:58 +0000

    Apr 4, 2023, 22:11:58 GMT+9:30

--- query3-5.sql
+++ query3-5.sql
@@ -1,5 +1,5 @@
 SELECT Students.given_name, Students.family_name, Enrolments.mark
-FROM Subjects
-INNER JOIN Enrolments
-ON Enrolments.subject_code = Subjects.subject_code
-WHERE subject = 'Computer Systems'
+FROM Enrolments
+INNER JOIN Students ON Enrolments.student_id = Students.student_id
+INNER JOIN Subjects ON Enrolments.subject_code = Subjects.subject_code
+WHERE Subjects.subject = 'Computer Systems' AND Enrolments.mark < 50;
