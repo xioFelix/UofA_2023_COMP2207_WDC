456ca28e69f55f8dc3fe0860ebe5898bff2274d1
Felix <a1878510@adelaide.edu.au>
2023-04-06 14:12:52 +0000

    Test

--- task2.sql
+++ task2.sql
@@ -26,7 +26,6 @@ USE `enrolment`;
 --
 -- Table structure for table `Enrolments`
 --
-
 DROP TABLE IF EXISTS `Enrolments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
