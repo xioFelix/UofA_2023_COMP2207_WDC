628007ff98a1bface9cc37fe28f3d154628b1447
Felix <a1878510@adelaide.edu.au>
2023-05-12 15:21:36 +0000

    May 13, 2023, 00:51:36 GMT+9:30

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post('/pass-it-on', (req, res) => {
     if (!message || message.trim() === '') {
         res.status(400).send('Bad Request');
     } else {
-        const responseMessage = previousMessage || 'No previous message available.';
+        const responseMessage = preMessage || 'No previous message available.';
         preMessage = message;
         res.send(responseMessage);
     }
