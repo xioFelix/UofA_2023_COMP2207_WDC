716df31589db24b7a97203cd71183ac7942ee6b5
Felix <a1878510@adelaide.edu.au>
2023-05-16 03:58:17 +0000

    May 16, 2023, 13:28:17 GMT+9:30

--- app.js
+++ app.js
@@ -46,6 +46,17 @@ app.post('/pass-it-on', (req, res) => {
     }
 });
 
+/* 1.3 */
+app.post('/combine', (req, res) => {
+    const firstInput = req.body.firstInput;
+    const secondTextArea = req.body.secondTextArea;
+
+    const output = firstInput + "\n" + secondTextArea;
+
+    res.send(output);
+});
+
+
 /* npm status show */
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
