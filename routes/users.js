30b4ff5e79d297bee88b35c09e749c891a191417
Felix <a1878510@adelaide.edu.au>
2023-05-16 06:36:31 +0000

    May 16, 2023, 16:06:31 GMT+9:30

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* 2.2 */
-import app from "../app";
+import app from "../app.js";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
