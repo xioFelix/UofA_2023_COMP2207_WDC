9fe77e642f7e2f615439a3ac78d9a9ff29a75735
Felix <a1878510@adelaide.edu.au>
2023-05-16 05:14:05 +0000

    May 16, 2023, 14:44:05 GMT+9:30

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* 
+/* 1.4 */
+let posts = [];
+
+// 处理POST请求
+router.post('/addpost', (req, res) => {
+  const post = req.body;
+  posts.unshift(post); // 将新的博客文章添加到数组的开头
+  res.status(200).send();
+});
+
+// 处理GET请求
+router.get('/getposts', (req, res) => {
+  res.json(posts); // 将所有博客文章的数据以JSON格式发送回客户端
+});
+
+module.exports = router;
+
+
 module.exports = router;
