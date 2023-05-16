103172f36ade2af19d61ea741597d5a8030fd71d
Felix <a1878510@adelaide.edu.au>
2023-05-16 03:58:21 +0000

    May 16, 2023, 13:28:21 GMT+9:30

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.post('/pass-it-on', (req, res) => {
 
 /* 1.3 */
 app.post('/combine', (req, res) => {
-    const firstInput = req.body.firstInput;
-    const secondTextArea = req.body.secondTextArea;
+    const {firstInput} = req.body;
+    const {secondTextArea} = req.body;
 
     const output = firstInput + "\n" + secondTextArea;
 
