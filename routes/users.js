59fadfbad9da5abcb54ccb45aa92bec134c80d43
Felix <a1878510@adelaide.edu.au>
2023-05-16 07:47:33 +0000

    May 16, 2023, 17:17:33 GMT+9:30

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* 2.2 */
 const logPostRequests = (req, res, next) => {
   if (req.method === "POST") {
     console.log("POST from a user");
   }
   next();
 };
+
 router.use(logPostRequests);
 
-/* Init */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-/* 1.4 */
 let posts = [];
 router.post('/addpost', (req, res) => {
   const post = req.body;
@@ -26,10 +24,5 @@ router.post('/addpost', (req, res) => {
 router.get('/getposts', (req, res) => {
   res.json(posts);
 });
-
-/* 3.1 */
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
 
 module.exports = router;
