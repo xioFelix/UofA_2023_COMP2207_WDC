6acacc23fca28252791208d923781de159c240d4
Felix <a1878510@adelaide.edu.au>
2023-05-21 12:52:14 +0000

    May 21, 2023, 22:22:14 GMT+9:30

--- query1-4.sql
+++ query1-4.sql
@@ -1,6 +1,5 @@
 SELECT address
 FROM address
-JOIN address ON store.address_id = store.store_id
 JOIN store ON address.address_id = store.address_id
 JOIN inventory ON store.store_id = inventory.store_id
 JOIN film ON inventory.film_id = film.film_id
