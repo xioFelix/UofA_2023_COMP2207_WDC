765a06df54762c05d02559f2cd809f0861c5580c
Felix <a1878510@adelaide.edu.au>
2023-05-21 23:56:52 +0930

    Update

--- app.js
+++ app.js
@@ -17,4 +17,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+const mysql = require('mysql');
+
+// 创建与数据库的连接
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'actors'
+});
+
+// 设置中间件来解析请求主体
+app.use(express.json());
+
+// 处理查询所有演员的请求
+app.get('/actors', (req, res) => {
+    // 执行数据库查询
+    connection.query('SELECT * FROM actors', (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// 处理添加演员的请求
+app.post('/actors', (req, res) => {
+    const { firstName, lastName } = req.body;
+
+    // 执行数据库插入操作
+    connection.query('INSERT INTO actors (firstName, lastName) VALUES (?, ?)', [firstName, lastName], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json({ id: result.insertId, firstName, lastName });
+        }
+    });
+});
+
+
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-app.get('/actors', (req, res) => {
-  // 处理查询所有演员的请求
-  // 在此处执行数据库查询并返回演员数据的JSON响应
-});
-
-app.post('/actors', (req, res) => {
-  // 处理添加演员的请求
-  // 在此处获取POST请求中的演员数据，并将其存储到数据库中
-  // 返回适当的响应，如成功添加的演员数据的JSON响应
-});
-
-
 module.exports = router;
