4335bc99066389c0ba49c4e330f19a3839951e6e
Felix <a1878510@adelaide.edu.au>
2023-05-21 13:32:33 +0000

    May 21, 2023, 23:02:33 GMT+9:30

--- query1-5.sql
+++ query1-5.sql
@@ -1,4 +1,4 @@
-SELECT SUM(film.duration) AS total_duration
+SELECT SUM(rental_duration) AS total_duration
 FROM film
 JOIN film_actor ON film.film_id = film_actor.film_id
 JOIN actor ON film_actor.actor_id = actor.actor_id
