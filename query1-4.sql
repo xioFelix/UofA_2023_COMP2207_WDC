4b65346b753e4c0b20c0cca9071a4d05c9092840
Felix <a1878510@adelaide.edu.au>
2023-05-21 12:47:50 +0000

    May 21, 2023, 22:17:50 GMT+9:30

--- query1-4.sql
+++ query1-4.sql
@@ -1,6 +1,6 @@
 SELECT address
 FROM address
-JOIN store ON store.address_id = store.store_id
+JOIN address ON store.address_id = store.store_id
 JOIN inventory ON address.store_id = inventory.store_id
 JOIN film ON inventory.film_id = film.film_id
 WHERE film.title = 'TWISTED PIRATES'
