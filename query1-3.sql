412fe48be44f7ea96bfba758491dfe37a71088c2
Felix <a1878510@adelaide.edu.au>
2023-05-21 22:56:31 +0930

    Update

--- query1-3.sql
+++ query1-3.sql
@@ -2,5 +2,5 @@ SELECT first_name, last_name, rental_date
 FROM customer
 JOIN rental ON customer.customer_id = rental.customer_id
 WHERE return_date IS NULL
-ORDER BY rental_date DESC
+ORDER BY rental_date ASC
 LIMIT 1;
